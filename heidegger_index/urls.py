fb87f8e3da801c03a7115db4e6a5b300c36682e5
André van Delft <andrevandelft@outlook.com>
2021-04-27 10:54:31 +0200

    Adjust base url

--- heidegger_index/urls.py
+++ heidegger_index/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from heidegger_index.views import index_view
 
 urlpatterns = [
-    path('', index_view),
+    path('', RedirectView.as_view(url='heidegger-index')),
+    path('heidegger-index', index_view),
 ]
