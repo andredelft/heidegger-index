17aaad9084b33f0cab9b20ab8628180a002efab6
André van Delft <andrevandelft@outlook.com>
2022-01-30 18:29:17 +0100

    Use namespaced URL names

--- heidegger_index/urls.py
+++ heidegger_index/urls.py
@@ -4,13 +4,20 @@ from django.conf import settings
 
 from heidegger_index.views import LemmaDetailView, index_view, WorkDetailView
 
-heidegger_index_patterns = [
-    path("", index_view, name="home"),
-    path("work/<slug>", WorkDetailView.as_view(), name="work-detail"),
-    path("lemma/<slug>", LemmaDetailView.as_view(), name="lemma-detail"),
-]
+namespaced_patterns = (
+    [
+        path("", index_view, name="home"),
+        path("work/<slug>", WorkDetailView.as_view(), name="work-detail"),
+        path("lemma/<slug>", LemmaDetailView.as_view(), name="lemma-detail"),
+    ],
+    "index",
+)
 
-urlpatterns = [
-    path("", RedirectView.as_view(url=settings.URL_PREFIX)),
-    path(settings.URL_PREFIX, include(heidegger_index_patterns)),
-]
+urlpatterns = (
+    [
+        path("", RedirectView.as_view(url=settings.URL_PREFIX)),
+        path(settings.URL_PREFIX, include(namespaced_patterns)),
+    ]
+    if settings.URL_PREFIX
+    else [path("", include(namespaced_patterns))]
+)
