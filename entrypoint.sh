310a00e4b63be146398c19ef93b4a4dc657de0fa
André van Delft <andrevandelft@outlook.com>
2022-02-03 10:34:01 +0100

    Fix entrypoint command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
-python manage.py populate
+python manage.py populate_index
 python manage.py collectstatic --no-input
 gunicorn heidegger_index.wsgi:application --bind 0.0.0.0:$1
