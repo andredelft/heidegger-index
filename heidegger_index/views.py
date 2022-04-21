188f36e3f85b67d3f03f7e1860f4e26dff4cc750
Johannes de Wit <77152231+johannesdewit@users.noreply.github.com>
2022-04-21 13:40:11 +0200

    Add similar-lemmata section to the lemma-detail view. Based on index.find_ref.

--- heidegger_index/views.py
+++ heidegger_index/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 
 from heidegger_index.models import Lemma, Work, PageReference
+from heidegger_index.settings import BASE_DIR
+from fuzzysearch import find_near_matches
+import yaml
 
 
 def index_view(request):
@@ -40,3 +43,36 @@ class LemmaDetailView(DetailView):
     model = Lemma
     template_name = "lemma_detail.html"
     context_object_name = "lemma"
+
+    # Just a basic copy of index.py find_ref
+    def _find_similar_lemmata(self, subject_lemma: Lemma, max_l_dist=2, num_results=3):
+        search_term = subject_lemma.value
+
+        with open(BASE_DIR / "index" / "heidegger-index.yml") as f:
+            lemmata = yaml.load(f, Loader=yaml.FullLoader).keys()
+
+        # Remove subject lemma from list
+        lemmata = [l for l in lemmata if l != search_term]
+
+        matches = [
+            (lemma, find_near_matches(search_term, lemma, max_l_dist=max_l_dist))
+            for lemma in lemmata
+        ]
+
+        # Remove unmatched lemmata from list
+        matches = [m for m in matches if m[1]]
+
+        # Sort by levensteihn distance first, then by lemma
+        matches.sort(key=lambda match: (min(m.dist for m in match[1]), match[0]))
+
+        similar_lemmata = []
+        for match in matches[:num_results]:
+            similar_lemmata.append(Lemma.objects.get(value=match[0]))
+
+        # returns list of similar lemma objects
+        return similar_lemmata
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["similar_lemmata"] = self._find_similar_lemmata(context["lemma"])
+        return context
