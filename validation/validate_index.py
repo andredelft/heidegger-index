b0506d77b1a47665ca4c471351c167fb120a4e13
André van Delft <andrevandelft@outlook.com>
2022-05-19 11:52:48 +0200

    Fix validation mistake

--- validation/validate_index.py
+++ validation/validate_index.py
@@ -9,13 +9,16 @@ SCHEMA_FILE = WORKING_DIR / "schema.yml"
 
 yaml.warnings({"YAMLLoadWarning": False})
 
+
 def validate_yaml(file, schema):
-    validate(
-        yaml.load(file),
-        yaml.load(schema)
-        )
+    validate(yaml.load(file), yaml.load(schema))
+
 
 def validate_index():
     with open(INDEX_FILE) as f:
         with open(SCHEMA_FILE) as s:
             validate_yaml(f, s)
+
+
+if __name__ == "__main__":
+    validate_index()
