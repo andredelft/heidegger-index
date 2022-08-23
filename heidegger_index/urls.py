33b1d9b95ad9154a4413efa02545ff90aa171b37
Johannes de Wit <77152231+johannesdewit@users.noreply.github.com>
2022-08-23 15:12:34 +0200

    Allow browsing the index by URN. E.g. https://heidegger.delve.nu/index/lemma/urn:cts:greekLit:tlg0626 will redirect to https://heidegger.delve.nu/index/lemma/heraklit

--- heidegger_index/urls.py
+++ heidegger_index/urls.py
@@ -1,13 +1,18 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
 from django.conf import settings
 
-from heidegger_index.views import LemmaDetailView, index_view, WorkDetailView
+from heidegger_index.views import LemmaDetailView, index_view, WorkDetailView, URNRedirectView
 
 namespaced_patterns = (
     [
         path("", index_view, name="home"),
         path("work/<slug>", WorkDetailView.as_view(), name="work-detail"),
+        re_path(
+            r"^lemma/(?P<urn>urn:cts:([A-Za-z0-9()+,\-.:=@;$_!*']|%[0-9A-Fa-f]{2})+)", 
+            URNRedirectView.as_view(),
+            name="urn-redirect"
+        ),
         path("lemma/<slug>", LemmaDetailView.as_view(), name="lemma-detail"),
     ],
     "index",
--- heidegger_index/views.py
+++ heidegger_index/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
+from django.views.generic.base import RedirectView
 from django.shortcuts import redirect
 from django.conf import settings
 
@@ -107,3 +108,8 @@ class LemmaDetailView(DetailView):
             context["works"] = lemma.works.all()
             context["author_short"] = lemma.value.split(",")[0]
         return context
+
+class URNRedirectView(LemmaDetailView):
+    def get(self, *args, **kwargs):
+        lemma = get_object_or_404(Lemma, urn=kwargs['urn'])
+        return redirect("index:lemma-detail", slug=lemma.slug)
