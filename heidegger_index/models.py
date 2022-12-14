09515ee596d163c9c6e2a4ae299d9fdcc6d76f07
Johannes de Wit <77152231+johannesdewit@users.noreply.github.com>
2022-12-14 17:37:45 +0100

    Revert "Rewrite xml parsing for Perseus."
    This reverts commit 89d0397dc42e0b7d685d1ea81922d1ba27dc118b.

--- heidegger_index/models.py
+++ heidegger_index/models.py
@@ -95,12 +95,8 @@ class Lemma(models.Model):
             else:
                 p_link = 'https://scaife-cts.perseus.org/api/cts?request=GetPassage&urn=' + self.urn
                 p_response = requests.get(p_link)
-                parsed_xml = BeautifulSoup(p_response.text, 'lxml')
-                while parsed_xml.p.bibl:
-                    parsed_xml.p.bibl.decompose()
-                while parsed_xml.p.label:
-                    parsed_xml.p.label.decompose()
-                self.perseus_content = str(parsed_xml.p.get_text())
+                parsed_xml = BeautifulSoup(p_response.text, 'html.parser')
+                self.perseus_content = parsed_xml.p.contents[-1].string
 
     class Meta:
         ordering = ["sort_key"]
